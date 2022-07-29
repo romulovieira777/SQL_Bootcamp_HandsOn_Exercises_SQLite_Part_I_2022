cf294278d9a9ec3ad7a0822512e6accd44f62630
Romulo Vieira <romulo.vieira777@gmail.com>
2022-07-29 09:38:15 -0300

    Select

---
+++ Section_03_Select/01_Exercise.sql
@@ -0,0 +1,6 @@
+/* 
+Using only SQL statement, display the result of the operation betow:
+    - 20 + 30
+*/
+SELECT
+    20 + 30;
---
+++ Section_03_Select/02_Exercise.sql
@@ -0,0 +1,8 @@
+/*
+Using only THE Select statement, display the result of the operation betow:
+    - 20 * 30
+
+Assign an alias result.
+*/
+SELECT
+    20 * 30 AS result;
