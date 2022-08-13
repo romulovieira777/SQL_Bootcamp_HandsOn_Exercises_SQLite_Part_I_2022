ef7f411f20da40d9634ac2cc37834b8a7c72821b
Romulo Vieira <romulo.vieira777@gmail.com>
2022-08-13 11:55:03 -0300

    SQLite + Python

---
+++ Section_25_SQLite_Python/Python/01_Exercise.py
@@ -0,0 +1,25 @@
+"""
+Exercise No. 01
+
+Using the sqlite3 package connect to the northwind database.
+
+Then create a Curso object and execute the following SQL query to the database:
+
+    SELECT * FROM category;
+
+Using the appropriate method, print the first row from the above query to the console. Finally, close the database
+connection.
+"""
+import sqlite3
+
+conn = sqlite3.connect('northwind.db')  # Connect to the database
+cursor = conn.cursor()  # Create a cursor object
+
+cursor.execute("SELECT * FROM category;")  # Execute the query
+
+# row = cursor.fetchone()  # Fetch the first row
+
+# print(row)  # Print the first row
+print(cursor.fetchone())  # Print the first row
+
+conn.close()  # Close the connection to the database
